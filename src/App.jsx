a543cf84d36d5f3908019b6d1e8b1305c1466ef8
Billy Castro <billycastro.1822@gamil.com>
2024-08-21 13:16:10 -0500

    vid 80 aumentando y disminuyendo el cart dinamicamente

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Header from './COMPONENTS/Header'
 import './App.css'
 import Guitar from './COMPONENTS/Guitar'
 import { db } from './Data/db'
-function App() {
-  const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
-
+  function App() {
+    const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
+    const max_value = 5;
+    const min_value = 1;
   function addToCard(item){
     const itemExist = cart.findIndex(guitar => guitar.id === item.id)
     if(itemExist >=0){
+      if(cart[itemExist].quantity >= max_value) return
       const updateCart = [...cart];
       updateCart[itemExist].quantity++;
       setCart(updateCart);
@@ -19,10 +21,43 @@ function App() {
     }
   }
 
+  function removeFromCart(id){
+    setCart(prevCart=> prevCart.filter(guitar=> guitar.id !== id))
+  }
+
+  function increaseQuantity(id){
+    const updateCard = cart.map( guitar => {
+      if(guitar.id == id && guitar.quantity < max_value){
+        return{
+          ...guitar,
+          quantity: guitar.quantity + 1
+        }
+      }
+      return guitar;
+    })
+    setCart(updateCard)
+  }
+
+  function decreaseQuantity(id){
+    const updateCard = cart.map( guitar => {
+      if(guitar.id == id && guitar.quantity > min_value){
+        return{
+          ...guitar,
+          quantity: guitar.quantity - 1
+        }
+      }
+      return guitar;
+    })
+    setCart(updateCard)
+  }
+
   return (
     <>
     <Header 
       cart={cart}
+      removeFromCart = {removeFromCart}
+      increaseQuantity= {increaseQuantity}
+      decreaseQuantity= {decreaseQuantity}
     />
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
